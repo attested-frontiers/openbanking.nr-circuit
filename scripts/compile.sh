69692eb78d41b909a2e60538e2f89d7ed0fec161
Ian-Bright <ianbrighton1@gmail.com>
2025-02-02 15:19:45 -0700

    feat: commit at transfer_to_public bug

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-cd ..
-nargo compile --force --silence-warnings
+get_token_contract_bytecode() {
+    token_bytecode_path="../../js/node_modules/@aztec/noir-contracts.js/artifacts/token_contract-Token.json"
+    cp $token_bytecode_path "contracts/openbank-escrow/target/token_contract-Token.json"
+    pwd
+}
+
+cd ../contracts/openbank-escrow
+# aztec-nargo compile --force --silence-warnings
+get_token_contract_bytecode

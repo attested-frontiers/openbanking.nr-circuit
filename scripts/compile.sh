e2d83abaecafe9346266b19fe29a8d8943058adf
Jack Gilcrest <me@jp4g.llc>
2025-02-02 17:51:49 -0700

    stash

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+
+# paths
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+CURRENT_DIR=$(pwd)
+TOKEN_BYTECODE_PATH=$SCRIPT_DIR/../js/node_modules/@aztec/noir-contracts.js/artifacts/token_contract-Token.json
+TOKEN_TXE_DESTINATION=$SCRIPT_DIR/../contracts/openbank-escrow/target/token_contract-Token.json
 
-get_token_contract_bytecode() {
-    token_bytecode_path="../../js/node_modules/@aztec/noir-contracts.js/artifacts/token_contract-Token.json"
-    cp $token_bytecode_path "contracts/openbank-escrow/target/token_contract-Token.json"
-    pwd
-}
+# yarn for the js
+cd $SCRIPT_DIR/../js
+yarn
 
-cd ../contracts/openbank-escrow
-# aztec-nargo compile --force --silence-warnings
-get_token_contract_bytecode
+# check the version of aztec
+EXPECTED_AZTEC_VERSION="0.72.1"
+AZTEC_VERSION=$(aztec --version | tr -d '\r\n')
+if [ "$AZTEC_VERSION" != "$EXPECTED_AZTEC_VERSION" ]; then
+    echo "Aztec version mismatch, installing version $EXPECTED_AZTEC_VERSION"
+    VERSION=$EXPECTED_AZTEC_VERSION aztec-up
+fi
+
+# compile the contracts
+cd $SCRIPT_DIR/../contracts/openbank-escrow
+aztec-nargo compile --force --silence-warnings
+
+# copy the token bytecode to the target folder for txe
+cp $TOKEN_BYTECODE_PATH $TOKEN_TXE_DESTINATION 
+
+cd $CURRENT_DIR
+

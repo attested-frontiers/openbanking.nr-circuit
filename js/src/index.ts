9419a8c9edd05773aaa63e7d9d4deee246c82c04
Jack Gilcrest <me@jp4g.llc>
2025-02-10 09:35:09 -0700

    pubkey retrieval and hashing set up

--- js/src/index.ts
+++ js/src/index.ts
@@ -3,3 +3,4 @@ export * from "./constants";
 export * from "./inputGen";
 export * from "./types";
 export * from "./utils";
+export * from "./pubkey";
--- js/tests/circuit.test.ts
+++ js/tests/circuit.test.ts
@@ -6,6 +6,8 @@ import {
   OpenBankingDomesticCircuit,
   decodeNoirOutputs,
   generateNoirInputs,
+  REVOLUT_JWKS_URI,
+  getPubkeyHashes
 } from '../src';
 
 // hardcoded inputs for now
@@ -14,6 +16,8 @@ const signature =
   '3e42c30cab535ed5a20dcac4d405004b5098451c72a80b4460b4e3e9a4bc89f131fa6078c1f7de1d740bfd8216e0ea8b67e5d78eaa7897d02902d73c50d3d0e7bbeb4e1b4b6b4d0281bcfb0e029c44f3ea90363e4e1d7ec591e09fc2bdd832428396b054f4f89336df49c01a88bb7e5b5015e706cd179467bf9794a79474884e799fb388050a7fdcaa074225bdc1b856048640e4fb7955a06675649acd89b049b603c0dc32dc5f37796453602f36cc982f86257055162457db6aec9377e7e9fdcb31e4ebce5d6e445c722f0e6a20936bda5c83481b12013078c0cc72551373586dc69db541d729b8d02521a26bb4f42068764438443e9c9164dca039b0fb1176';
 const { publicKey } = new X509Certificate(fs.readFileSync('./tests/test_data/revolut.cert', 'utf8'));
 
+const issuingCA = fs.readFileSync('./tests/test_data/revolut_issuing_ca.cer', 'utf8');
+const rootCA = fs.readFileSync('./tests/test_data/revolut_root_ca.cer', 'utf8');
 
 describe('OpenBanking.nr Circuit Test', () => {
   let noir: Noir
@@ -24,9 +28,9 @@ describe('OpenBanking.nr Circuit Test', () => {
   });
 
   describe('Simulate Witnesses', () => {
-    it('Test execution', async () => {
+    xit('Test execution', async () => {
       const inputs = generateNoirInputs(payload, signature, publicKey);
-      const result = await noir.execute({params: inputs });
+      const result = await noir.execute({ params: inputs });
       const outputs = decodeNoirOutputs(result.returnValue);
       console.log(outputs);
 
@@ -39,5 +43,18 @@ describe('OpenBanking.nr Circuit Test', () => {
       };
       expect(outputs).toEqual(expectedOutputs);
     });
+    it('CA', async () => {
+      const hashes = await getPubkeyHashes(REVOLUT_JWKS_URI).then(hashes => hashes.map(hash => hash.toString()));
+      const expectedHashes = [
+        "0x122fe470d24a14ba2e21e27225df5897b36e91e4ac6f62e022d4b901331b9ade",
+        "0x28a4057201b24bfdf7a37dd598077aee7cc969c1f58a3cf6efa28778ba1e35f6",
+        "0x0e2c0956cae7472177eb75f5b7228057b0bca4a15d2f97b3416e4b6dbda116c6",
+        "0x063b9064814feb22c88e0261a7028adeaec3acd41cb92336c3be712dd1bf0a92",
+        "0x1995a9a76e9e03a897b202b27e9a71a7513b733e1a19bbb40cc3f81e2fb2b23f"
+      ];
+      for (let i = 0; i < hashes.length; i++) {
+        expect(hashes[i]).toEqual(expectedHashes[i]);
+      }
+    })
   });
 });

97307b882ab22914835fcc77b92440bd449805b0
Jack Gilcrest <me@jp4g.llc>
2025-02-10 10:50:20 -0700

    add (untested) pubkey registry

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+TOKEN_BYTECODE_PATH=$SCRIPT_DIR/../js/node_modules/@aztec/noir-contracts.js/artifacts/token_contract-Token.json
+TXE_TOKEN_PATH=$SCRIPT_DIR/../contracts/openbanking-escrow/target/token_contract-Token.json
 
-get_token_contract_bytecode() {
-    token_bytecode_path="../../js/node_modules/@aztec/noir-contracts.js/artifacts/token_contract-Token.json"
-    cp $token_bytecode_path "contracts/openbank-escrow/target/token_contract-Token.json"
-    pwd
-}
+cd $SCRIPT_DIR/../contracts/openbanking-escrow
 
-cd ../contracts/openbank-escrow
-# aztec-nargo compile --force --silence-warnings
-get_token_contract_bytecode
+aztec-nargo compile --force --silence-warnings
+cp $TOKEN_BYTECODE_PATH $TXE_TOKEN_PATH

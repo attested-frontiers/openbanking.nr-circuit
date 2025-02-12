9ea1d753e8492be3ad5129687d5efa0b43ec3438
Ian-Bright <ianbrighton1@gmail.com>
2025-02-12 15:50:25 +0000

    feat: Deploy script added

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+ARTIFACTS_PATH=$SCRIPT_DIR/../js/src/artifacts/contracts
+COMPILED_CONTRACT_NAME=openbanking_escrow-OpenbankingEscrow.json
 TOKEN_BYTECODE_PATH=$SCRIPT_DIR/../js/node_modules/@aztec/noir-contracts.js/artifacts/token_contract-Token.json
 TXE_TOKEN_PATH=$SCRIPT_DIR/../contracts/openbanking-escrow/target/token_contract-Token.json
 
 cd $SCRIPT_DIR/../contracts/openbanking-escrow
 
 aztec-nargo compile --force --silence-warnings
+aztec codegen ./target/$COMPILED_CONTRACT_NAME -o $ARTIFACTS_PATH
+cp ./target/$COMPILED_CONTRACT_NAME $ARTIFACTS_PATH
 cp $TOKEN_BYTECODE_PATH $TXE_TOKEN_PATH

54bdf34e7da1f5c2ddecbc84818c684ee32febeb
Ian-Bright <ianbrighton1@gmail.com>
2025-04-22 19:19:14 -0600

    feat: Update deploy script to use fee juice sponsor contract

--- js/src/deploy.ts
+++ js/src/deploy.ts
@@ -1,11 +1,19 @@
 import {
+    AccountWalletWithSecretKey,
+    ContractInstanceWithAddress,
     createPXEClient,
+    Fq,
     Fr,
+    getContractInstanceFromDeployParams,
+    PXE,
+    SponsoredFeePaymentMethod,
     waitForPXE,
 } from '@aztec/aztec.js';
-import { getUnsafeSchnorrAccount } from '@aztec/accounts/single_key';
+import { getSchnorrAccount } from '@aztec/accounts/schnorr';
 import { TokenContract } from '@aztec/noir-contracts.js/Token';
 import { OpenbankingEscrowContract } from './artifacts/contracts/OpenbankingEscrow.js';
+import { SPONSORED_FPC_SALT } from '@aztec/constants';
+import { SponsoredFPCContract } from '@aztec/noir-contracts.js/SponsoredFPC';
 
 const USDC_TOKEN = {
     symbol: 'USDC',
@@ -13,29 +21,8 @@ const USDC_TOKEN = {
     decimals: 6,
 };
 
-/**
- * Deploys a new token contract and escrow contract to the specified PXE URL
- */
-const deploy = async () => {
-    // const pxe = createPXEClient('https://pxe.obsidion.xyz');
-    const pxe = createPXEClient('http://localhost:8080');
-    await waitForPXE(pxe);
-
-    // @ts-ignore
-    const admin = await getUnsafeSchnorrAccount(pxe, Fr.random(), 0);
-    const adminWallet = await admin.waitSetup();
-
-    const token = await TokenContract.deploy(
-        adminWallet,
-        adminWallet.getAddress(),
-        USDC_TOKEN.symbol,
-        USDC_TOKEN.name,
-        USDC_TOKEN.decimals
-    )
-        .send()
-        .deployed();
-
-    const escrow = await OpenbankingEscrowContract.deploy(
+const deployEscrowContract = async (adminWallet: AccountWalletWithSecretKey, paymentMethod: SponsoredFeePaymentMethod, token: TokenContract): Promise<OpenbankingEscrowContract> => {
+    return await OpenbankingEscrowContract.deploy(
         adminWallet,
         token.address,
         [
@@ -56,19 +43,70 @@ const deploy = async () => {
             ),
         ]
     )
-        .send()
+        .send({ fee: { paymentMethod } })
         .deployed();
+}
 
-    console.log('Token contract: ', token.address.toString());
-    console.log('Escrow contract: ', escrow.address.toString());
-    console.log('Token admin: ', adminWallet.getSecretKey().toString());
+const deployTokenContract = async (adminWallet: AccountWalletWithSecretKey, paymentMethod: SponsoredFeePaymentMethod): Promise<TokenContract> => {
+    return await TokenContract.deploy(
+        adminWallet,
+        adminWallet.getAddress(),
+        USDC_TOKEN.symbol,
+        USDC_TOKEN.name,
+        USDC_TOKEN.decimals,
+    )
+        .send({ fee: { paymentMethod } })
+        .deployed();
+}
 
-    // await registerContractClassArtifact(
-    //     'EscrowContract',
-    //     OpenbankingEscrowArtifactJson as NoirCompiledContract,
-    //     escrow.instance.contractClassId.toString(),
-    //     1,
-    // );
+const getSponsoredFPCInstance = async (): Promise<ContractInstanceWithAddress> => {
+    return await getContractInstanceFromDeployParams(SponsoredFPCContract.artifact, {
+        salt: new Fr(SPONSORED_FPC_SALT),
+    });
+}
+
+const getSponsoredFPCAddress = async () => {
+    return (await getSponsoredFPCInstance()).address;
+}
+
+const getDeployedSponsoredFPCAddress = async (pxe: PXE) => {
+    const fpc = await getSponsoredFPCAddress();
+    const contracts = await pxe.getContracts();
+    if (!contracts.find(c => c.equals(fpc))) {
+        throw new Error('SponsoredFPC not deployed.');
+    }
+    return fpc;
+}
+
+/**
+ * Deploys a new token contract and escrow contract to the specified PXE URL. Also grabs
+ * FPC address
+ */
+const deploy = async () => {
+    // const pxe = createPXEClient('https://pxe.obsidion.xyz');
+    const pxe = createPXEClient('http://localhost:8080');
+    await waitForPXE(pxe);
+
+
+
+    const secretKey = Fr.random();
+    const signingKey = Fq.random();
+
+    const fpc = await getDeployedSponsoredFPCAddress(pxe);
+    const paymentMethod = new SponsoredFeePaymentMethod(fpc);
+
+    // @ts-ignore
+    const admin = await getSchnorrAccount(pxe, secretKey, signingKey, 0);
+    const adminWallet = await admin.waitSetup({ fee: { paymentMethod } });
+
+    const token = await deployTokenContract(adminWallet, paymentMethod);
+    const escrow = await deployEscrowContract(adminWallet, paymentMethod, token);
+
+    console.log(`VITE_APP_ESCROW_CONTRACT_ADDRESS = "${escrow.address.toString()}"`);
+    console.log(`VITE_APP_TOKEN_CONTRACT_ADDRESS = "${token.address.toString()}"`);
+    console.log(`VITE_APP_TOKEN_ADMIN_SECRET_KEY = "${secretKey.toString()}"`);
+    console.log(`VITE_APP_TOKEN_ADMIN_SIGNING_KEY = "${signingKey.toString()}"`);
+    console.log(`VITE_APP_FPC_ADDRESS = "${fpc.toString()}"`)
 };
 
 deploy();
